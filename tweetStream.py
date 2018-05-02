6aab51a4671583f48c147977e2b987eb7dc5fcc9
jonasbiegert <jonas.biegert@gmx.de>
2018-05-02 11:56:53 +0200

    Update tweetStream.py

--- tweetStream.py
+++ tweetStream.py
@@ -1,16 +1,37 @@
 import tweepy
 
 #ich hab mal die access keys raus, die musst du dann wieder einfügen
-
 class TwitterStreamListener(tweepy.StreamListener):
     """ A listener handles tweets are the received from the stream.
     This is a basic listener that just prints received tweets to stdout.
     """
 
-    def on_status(self, status):
-        #getTweet(status)
-        TweetIDtoTxt(status)
+    def __init__(self):
+        super().__init__()
+        self.UnfilteredCount = 0
+        self.FilteredCount = 0
+        self.limit = 10
+        self.receivedTweets = "0"
 
+    def on_data(self, data):
+        data_json = json.loads(data)
+        TweetToJson(data_json)
+        self.FilteredCount += 1
+        if self.FilteredCount < self.limit:
+            return True
+        else:
+            stream.disconnect()
+
+
+
+    def on_status(self, status):
+        getTweet(status)
+        self.count += 1
+        if self.count < self.limit:
+            return True
+        else:
+            stream.disconnect()
+        #TweetIDtoTxt(status)
 
 
 
@@ -18,6 +39,9 @@ class TwitterStreamListener(tweepy.StreamListener):
         if status_code == 403:
             print("The request is understood, but it has been refused or access is not allowed. Limit is maybe reached")
             return False
+
+
+tweetCount = 0;
 
 def getTweet(tweet):
     if len(tweet.text) >= 50:
@@ -27,7 +51,12 @@ def getTweet(tweet):
 
 def TweetIDtoTxt(tweet):
     file = open("swe.txt", "a+")
-    file.write(tweet.id_str)
+    file.write(tweet.id_str + "\n")
+
+def TweetToJson(tweet):
+    if len(tweet.text) >= 50:
+        file = open("swe.json", "a+")
+        file.write(str (tweet))
 
 # Authentication
 auth = tweepy.OAuthHandler(consumerKey, consumerSecret) #logs in to twitter
@@ -42,7 +71,7 @@ stream = tweepy.Stream(auth=api.auth, listener=streamListener)
 try:
 
 
-    stream.filter(languages= ['sv'], track=['och', 'på', 'som'])
+    stream.filter(languages= ['sv'], track=['och', 'på', 'som'], async = 'true')
 
 except:
     print("search interrupted")
